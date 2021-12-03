1677e7c4cfad8d6e26e8d100a6ea1c7a22cd2c1a
Thomas Skovlund Hansen <thomasskovlundhansen@gmail.com>
2021-12-03 10:12:06 +0100

    Add 2021-3

---
+++ 2021/3a.py
@@ -0,0 +1,24 @@
+import sys
+import os
+
+filename = os.path.basename(__file__).split(".")[0][:-1] + ".txt"
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
+
+with open(filename) as f:
+    numbers = f.readlines()
+    gamma = [0] * (len(numbers[0]) - 1)
+    epsilon = [0] * (len(numbers[0]) - 1)
+    for number in numbers:
+        for i, bit in enumerate(number[:-1]):
+            gamma[i] += int(bit)
+    for i, bit in enumerate(gamma):
+        if gamma[i] > len(numbers) / 2:
+            gamma[i] = 2**(len(gamma) - i - 1)
+            epsilon[i] = 0
+        else:
+            epsilon[i] = 2**(len(gamma) - i - 1)
+            gamma[i] = 0
+    gamma = sum(gamma)
+    epsilon = sum(epsilon)
+    print(gamma * epsilon)
---
+++ 2021/3b.py
@@ -0,0 +1,35 @@
+import sys
+import os
+
+
+filename = os.path.basename(__file__).split(".")[0][:-1] + ".txt"
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
+
+def to_decimal(b):
+    d = 0
+    for i, bit in enumerate(b):
+        d += int(bit) * 2**(len(b) - i - 1)
+    return d
+
+with open(filename) as f:
+    oxy_numbers = f.readlines()
+    co2_numbers = oxy_numbers
+    i = 0
+    while len(oxy_numbers) > 1: 
+        s = 0
+        for number in oxy_numbers:
+            s += int(number[i])
+        s = int(s >= len(oxy_numbers) / 2)
+        oxy_numbers = list(filter(lambda n: int(n[i]) == s, oxy_numbers))
+        i += 1
+    i = 0
+    while len(co2_numbers) > 1:
+        s = 0
+        for number in co2_numbers:
+            s += int(number[i])
+        s = int(s < len(co2_numbers) / 2)
+        co2_numbers = list(filter(lambda n: int(n[i]) == s, co2_numbers))
+        i += 1
+    print(to_decimal(oxy_numbers[0][:-1]) * to_decimal(co2_numbers[0][:-1]))
+
