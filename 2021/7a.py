1a85a19f2c08d2020aa54419006cd3fc3d04b7be
Thomas Skovlund Hansen <thomasskovlundhansen@gmail.com>
2021-12-07 08:32:30 +0100

    Add 2021-7

---
+++ 2021/7a.py
@@ -0,0 +1,12 @@
+import sys
+import os
+from statistics import median
+
+filename = os.path.basename(__file__).split(".")[0][:-1] + ".txt"
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
+
+with open(filename) as f:
+    line = list(map(int, f.readline().split(",")))
+    m = median(line)
+    print(int(sum(map(lambda i: abs(i - m), line))))
---
+++ 2021/7b.py
@@ -0,0 +1,27 @@
+import sys
+import os
+from statistics import mean
+
+filename = os.path.basename(__file__).split(".")[0][:-1] + ".txt"
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
+
+def distance(l, m):
+    print(l, m)
+    distances = map(lambda i: abs(i - m) * (abs(i - m) + 1) // 2, l)
+    return int(sum(distances))
+
+with open(filename) as f:
+    line = list(map(int, f.readline().split(",")))
+    m = round(mean(line))
+
+    d = distance(line, m)
+    while distance(line, m - 1) < d:
+        m -= 1
+        d = distance(line, m)
+    while distance(line, m + 1) < d:
+        m += 1
+        d = distance(line, m)
+
+    print(d)
+        
