34f305dcff333eb5923259a170410f6c14a62f06
Thomas Skovlund Hansen <thomasskovlundhansen@gmail.com>
2021-12-21 09:44:43 +0100

    Update 2021-21 post

--- 2021/21b_post.py
+++ 2021/21b_post.py
@@ -11,30 +11,23 @@ if len(sys.argv) > 1:
 
 
 @functools.cache
-def wins(p1, p2, turn, s1, s2):
-    w1, w2 = [0, 0]
+def wins(p1, p2, s1, s2, turn):
+    total = 0
+    w = 0
     rolls = product(range(1, 4), repeat=3)
-    if turn == 0:
-        for roll in rolls:
-            p = (p1 + (sum(roll) - 1)) % 10 + 1
-            if s1 + p >= 21:
-                w1 += 1
-            else:
-                _w1, _w2 = wins(p, p2, 1, s1 + p, s2)
-                w1 += _w1
-                w2 += _w2
-    if turn == 1:
-        for roll in rolls:
-            p = (p2 + (sum(roll) - 1)) % 10 + 1
-            if s2 + p >= 21:
-                w2 += 1
-            else:
-                _w1, _w2 = wins(p1, p, 0, s1, s2 + p)
-                w1 += _w1
-                w2 += _w2
-    return w1, w2
+    for roll in rolls:
+        p = (p1 + (sum(roll) - 1)) % 10 + 1
+        if s1 + p >= 21:
+            w += turn
+            total += 1
+        else:
+            _w, _total = wins(p2, p, s2, s1 + p, not turn)
+            w += _w
+            total += _total
+    return w, total
 
 
 with open(filename) as f:
     p1, p2 = [int(l.strip("\n").split(": ")[1]) for l in f.readlines()]
-    print(wins(p1, p2, 0, 0, 0))
+    w, total = wins(p1, p2, 0, 0, True)
+    print(max(w, total - w))
