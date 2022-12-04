af4b40d4b83caef2f77ce9455462678b45ef100f
Thomas Skovlund Hansen <thomasskovlundhansen@gmail.com>
2022-12-04 08:38:48 +0100

    Add 2022-4

---
+++ 2022/4b.py
@@ -0,0 +1,8 @@
+from readinput import readfile
+
+sum = 0
+for line in readfile():
+    a, b = [list(map(int, i.split("-"))) for i in line.split(",")]
+    if a[0] <= b[0] and a[1] >= b[0] or b[0] <= a[0] and b[1] >= a[0]:
+        sum += 1
+print(sum)
