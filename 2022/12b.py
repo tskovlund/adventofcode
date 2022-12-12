48fdaeebd92ed4b118146a28c7c23681cd61bd5a
Thomas Skovlund Hansen <thomasskovlundhansen@gmail.com>
2022-12-12 08:39:50 +0100

    Add 2022-12

---
+++ 2022/12b.py
@@ -0,0 +1,67 @@
+from readinput import readfile
+
+lines = readfile()
+grid = []
+
+# Map heights to ints
+# and remember indices of S and E
+starts = []
+end = None
+for i, line in enumerate(lines):
+    grid.append([])
+    for j, c in enumerate(line):
+        if c == "S" or c == "a":
+            starts.append((i, j))
+            grid[i].append(0)
+        elif c == "E":
+            end = (i, j)
+            grid[i].append(ord("z") - ord("a"))
+        else:
+            grid[i].append(ord(c) - ord("a"))
+
+shortest_paths = []
+for start in starts:
+    # Do BFS
+    n = len(grid)
+    m = len(grid[0])
+    bfs = [(start[0], start[1], 0)]
+    visited = set()
+    node_ = None
+    while bfs:
+        i, j, l = bfs.pop()
+        if (i, j) in visited:
+            continue
+        h = grid[i][j]
+        if i > 0:
+            if grid[i - 1][j] <= h + 1:
+                node_ = (i - 1, j, l + 1)
+                if (node_[0], node_[1]) not in visited:
+                    if (node_[0], node_[1]) == end:
+                        break
+                    bfs = [node_] + bfs
+        if i < n - 1:
+            if grid[i + 1][j] <= h + 1:
+                node_ = (i + 1, j, l + 1)
+                if (node_[0], node_[1]) not in visited:
+                    if (node_[0], node_[1]) == end:
+                        break
+                    bfs = [node_] + bfs
+        if j > 0:
+            if grid[i][j - 1] <= h + 1:
+                node_ = (i, j - 1, l + 1)
+                if (node_[0], node_[1]) not in visited:
+                    if (node_[0], node_[1]) == end:
+                        break
+                    bfs = [node_] + bfs
+        if j < m - 1:
+            if grid[i][j + 1] <= h + 1:
+                node_ = (i, j + 1, l + 1)
+                if (node_[0], node_[1]) not in visited:
+                    if (node_[0], node_[1]) == end:
+                        break
+                    bfs = [node_] + bfs
+        visited.add((i, j))
+        node_ = None
+    if node_:
+        shortest_paths.append(node_[2])
+print(sorted(shortest_paths, reverse=True)[0])
