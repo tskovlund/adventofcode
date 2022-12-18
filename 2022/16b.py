d365e7cd725f19653d775949acb735f759e13c0c
Thomas Skovlund Hansen <thomasskovlundhansen@gmail.com>
2022-12-18 21:50:14 +0100

    Update 2022-16b to be fast instead of slow

--- 2022/16b.py
+++ 2022/16b.py
@@ -1,5 +1,6 @@
 from readinput import readfile_and_split
 from functools import cache
+from collections import defaultdict
 
 MINUTES = 26
 
@@ -16,43 +17,30 @@ flowvalves = frozenset({id for id in valves.keys() if valves[id][0] > 0})
 @cache
 def search(current="AA", minutes=MINUTES, open=frozenset(), openflow=0):
     if minutes == 0:
-        return {(0, open)}
+        return {open: 0}
     if open == flowvalves:
-        return {(minutes * openflow, open)}
+        return {open: minutes + openflow}
     flow, to = valves[current]
-    solutions = set()
-    for i, valve in enumerate(to):
-        if minutes == MINUTES:
-            print(i)
-        solutions_ = search(valve, minutes - 1, open, openflow)
-        solutions_ = {(openflow + solution[0], solution[1]) for solution in solutions_}
-        solutions = solutions.union(solutions_)
+    solutions = defaultdict(int)
+    for valve in to:
+        for open_, solution in search(valve, minutes - 1, open, openflow).items():
+            solutions[open_] = max(solutions[open_], solution + openflow)
     if current not in open and flow > 0:
         open = set(open)
         open.add(current)
-        solutions_ = search(current, minutes - 1, frozenset(open), openflow + flow)
-        solutions_ = {(openflow + solution[0], solution[1]) for solution in solutions_}
-        solutions = solutions.union(solutions_)
+        for open_, solution in search(
+            current, minutes - 1, frozenset(open), openflow + flow
+        ).items():
+            solutions[open_] = max(solutions[open_], solution + openflow)
     return solutions
 
 
-# Find all possible outcomes and find the best two solutions that don't have intersecting open sets
-solutions = sorted(
-    filter(lambda s: s[0] > 400, search()), key=lambda s: s[0], reverse=True
-)
-print(len(solutions))
-# Caution: The code below takes a long time to compute
-# but it returns the correct result and I don't feel like
-# optimizing it more right now even though I could probably
-# just iterate smarter through the sorted list :>
+# Find best solution for all open sets and find the
+# best two solutions that don't have intersecting open sets
+solutions = search().items()
 best = 0
-i = 0
-for s, o in solutions:
-    if i % 1000 == 0:
-        print(i)
-    i += 1
-    for s_, o_ in solutions:
-        if not o.intersection(o_):
-            best = max(best, s + s_)
-            break
+for open, solution in solutions:
+    for open_, solution_ in solutions:
+        if not open.intersection(open_):
+            best = max(best, solution + solution_)
 print(best)
